7db8b6a36f19224e0671a48538a688c983791f3f
Ransom Byers <ransom.byers@gmail.com>
2021-06-09 22:05:48 -0700

    Add Payroll.login().

--- payroll.py
+++ payroll.py
@@ -1,8 +1,21 @@
+import rpa as r
 import config as cfg
 
 class Payroll:
     def __init__(self):
-        self.url = cfg.payroll['url']
         self.user = cfg.payroll['user']
         self.password = cfg.payroll['password']
+        
+        self.url = 'https://www.heartlandplusone.com'
+        r.init()
+        
+    def __del__(self):
+        r.close()
+        
+    def login(self):
+        r.url(self.url)
+        r.type('//*[@id="ctl00_DefaultContent_Login1_UserName"]', self.user)
+        r.type('//*[@id="ctl00_DefaultContent_Login1_Password"]', self.password + "[enter]")
+        
+    
         
