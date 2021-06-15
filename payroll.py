ac353f3f410fdbd032fc1d358ec271899c60c7e5
Ransom Byers <ransom.byers@gmail.com>
2021-06-14 18:47:35 -0700

    Refactor Payroll.__init__, .__del__, and .login to use RPA Framework

--- payroll.py
+++ payroll.py
@@ -1,34 +1,35 @@
-import rpa as r
+# import rpa as r
+from RPA.Browser.Selenium import Selenium
 import config as cfg
 
 class Payroll:
     def __init__(self):
+        self.url = 'https://www.heartlandplusone.com'
         self.user = cfg.payroll['user']
         self.password = cfg.payroll['password']
-        self.url = 'https://www.heartlandplusone.com'
-#         r.init(visual_automation = True) 
-        r.init()
+        self.browser = Selenium()
         
     def __del__(self):
-        r.close()
+        self.browser.close_all_browsers()
         
     def login(self):
-        r.url(self.url)
-        r.type('//*[@id="ctl00_DefaultContent_Login1_UserName"]', self.user)
-        r.type('//*[@id="ctl00_DefaultContent_Login1_Password"]', self.password + "[enter]")
-        
-    def get_to_reports(self):
-        r.click('//*[@id="ctl00_h2"]') # "Reporting"
-        r.click('//*[@id="ctl00_c2_TreeViewt2"]') # "Consolidated Report Archive"
-        r.click('//*[contains(text(), "5/7/2021")]')
-        r.click('//*[@id="SelectAllCheckBox"]')
-        r.click('//*[@value="View Reports"]')
-        r.wait(20) # seconds
-        r.popup('ViewReport')
-#         r.click('//*[@id="download"]')
-#         r.popup() # returns to main
-#         r.keyboard('[crtl]S') # needs visual_automation
+        self.browser.open_available_browser(self.url)
+        self.browser.input_text('id:ctl00_DefaultContent_Login1_UserName', self.user)
+        self.browser.input_text('id:ctl00_DefaultContent_Login1_Password', self.password)
+        self.browser.press_keys('id:ctl00_DefaultContent_Login1_Password', 'ENTER')
+                
+#     def get_to_reports(self):
+#         r.click('//*[@id="ctl00_h2"]') # "Reporting"
+#         r.click('//*[@id="ctl00_c2_TreeViewt2"]') # "Consolidated Report Archive"
+#         r.click('//*[contains(text(), "5/7/2021")]')
+#         r.click('//*[@id="SelectAllCheckBox"]')
+#         r.click('//*[@value="View Reports"]')
+#         r.wait(20) # seconds
+#         r.popup('ViewReport')
+# #         r.click('//*[@id="download"]')
+# #         r.popup() # returns to main
+# #         r.keyboard('[crtl]S') # needs visual_automation
     
-    def r(self):
-        return r
+#     def r(self):
+#         return r
         
